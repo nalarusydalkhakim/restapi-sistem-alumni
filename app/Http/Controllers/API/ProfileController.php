451213a93742b609b1edb944c73fbbe9d802583e
bayubmtr <bayubimantoro27@gmail.com>
2022-05-24 12:58:33 +0700

    fix select profile

--- app/Http/Controllers/API/ProfileController.php
+++ app/Http/Controllers/API/ProfileController.php
@@ -21,6 +21,7 @@ class ProfileController extends Controller
     {
         $user = User::leftjoin('faculties', 'faculties.id', '=', 'users.faculty_id')
                         ->leftjoin('departements', 'departements.id', '=', 'users.departement_id')
+                        ->select('users.*', 'faculties.faculty_name', 'departements.departement_name')
                         ->findOrFail($id);
         $response = [
             'messege' => 'Detail of User Profile',
